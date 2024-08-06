374039aaccbf8e32bd6f1718311b9940d6b5ab8e
Iain Stewart <iain.stewart08@googlemail.com>
2024-08-06 09:21:43 +0100

    Setup Semantic UI

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {Header, List} from "semantic-ui-react";
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -14,12 +15,14 @@ function App() {
 
   return (
       <div>
-        <h1>Reactivities</h1>
-        <ul>
+        <Header as='h2' icon='users' content='Reactivities' />
+        <List>
           {activities.map((activity: any) => (
-              <li key={activity.id}>{activity.title}</li>
+              <List.Item key={activity.id}>
+                  {activity.title}
+              </List.Item>
           ))}
-        </ul>
+        </List>
       </div>
   )
 }
